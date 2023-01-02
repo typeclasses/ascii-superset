91aafd8faa8366ad8187447ca239ba3664b55885
Chris Martin <ch.martin@gmail.com>
2023-01-02 13:03:08 -0700

    ascii-case patch update

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@ sourceOverrides = haskell.lib.packageSourceOverrides {
 };
 
 depOverrides = new: old: {
-    ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
+    ascii-case = new.callPackage ./nix/ascii-case-1.0.1.2.nix {};
 };
 
 ghc."8.10" = nixos-22-05.haskell.packages.ghc8107.override (old: {
