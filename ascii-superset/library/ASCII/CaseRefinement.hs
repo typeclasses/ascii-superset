1be8050f256d26851abb471159fe0d2a8e4cd464
Chris Martin <ch.martin@gmail.com>
2023-01-05 16:53:31 -0700

    doc wibbles

--- ascii-superset/library/ASCII/CaseRefinement.hs
+++ ascii-superset/library/ASCII/CaseRefinement.hs
@@ -40,9 +40,9 @@ import Text.Show (Show, showList, showParen, showString, showsPrec)
 import qualified Data.Bool as Bool
 import qualified Data.List as List
 
-{- | This type constructor indicates that a value from some ASCII superset is
-valid ASCII, and also that any letters belong to a particular 'Case' indicated
-by the @letterCase@ type parameter.
+{-| Indicates that a value from some ASCII superset is valid ASCII, and also
+    that any letters belong to a particular 'Case' indicated by the @letterCase@
+    type parameter
 
 The @superset@ type parameter is the ASCII superset, which should be a type with
 an instance of either 'CharSuperset' or 'StringSuperset'.
@@ -85,7 +85,7 @@ instance Show superset => Show (ASCII'case letterCase superset) where
     showList x = showString "asciiCaseUnsafe " . showList (List.map lift x)
 
 {-| Change the type of an ASCII superset value that is known to be valid ASCII where
-letters are restricted to the 'Case' designated by the @letterCase@ type variable.
+    letters are restricted to the 'Case' designated by the @letterCase@ type variable
 
 This is "unsafe" because this assertion is unchecked, so this function is capable
 of producing an invalid 'ASCII'case' value. -}
@@ -97,10 +97,7 @@ asciiCaseUnsafe = ASCII'case_Unsafe
 {- $aliases
 
 The 'ASCII'upper' and 'ASCII'lower' type aliases exist primarily so that you can
-use 'ASCII'case' without the DataKinds language extension.
-
--}
-
+use 'ASCII'case' without the DataKinds language extension. -}
 type ASCII'upper superset = ASCII'case 'UpperCase superset
 
 type ASCII'lower superset = ASCII'case 'LowerCase superset
@@ -114,7 +111,7 @@ instance KnownCase 'LowerCase where theCase = LowerCase
 ---
 
 {-| Return 'Just' an 'ASCII'case' character if the input is an ASCII character
-in the proper case, or 'Nothing' otherwise -}
+    in the proper case, or 'Nothing' otherwise -}
 validateChar :: forall letterCase superset. KnownCase letterCase => CharSuperset superset =>
     superset {- ^ Character which may or may not be in the ASCII character set;
                   if a letter, may be in any case -}
@@ -124,8 +121,8 @@ validateChar x = do
     guard (Bool.not (Case.isCase (Case.opposite (theCase @letterCase)) c))
     Just (asciiCaseUnsafe x)
 
-{- | Return an 'ASCII'case' character if the input is an ASCII character in the
-proper case, or 'ASCII.Substitute' otherwise -}
+{-| Return an 'ASCII'case' character if the input is an ASCII character in the
+    proper case, or 'ASCII.Substitute' otherwise -}
 substituteChar :: forall letterCase superset. KnownCase letterCase => CharSuperset superset =>
     superset
     -> ASCII'case letterCase superset
@@ -134,15 +131,15 @@ substituteChar x = case validateChar x of
     Just c -> c
 
 {-| Lift a 'CaselessChar' into a superset type, wrapped in the 'ASCII'case'
-refinement to save the evidence that it is ASCII in a particular case -}
+    refinement to save the evidence that it is ASCII in a particular case -}
 fromCaselessChar :: forall letterCase superset. KnownCase letterCase => CharSuperset superset =>
     CaselessChar -- ^ Character which, if it is a letter, does not have a specified case
     -> ASCII'case letterCase superset
 fromCaselessChar = asciiCaseUnsafe . Superset.fromChar . Caseless.toCase (theCase @letterCase)
 
 {-| Given a character from some type that is known to represent an ASCII
-character in a particular case, obtain the caseless ASCII character it
-represents -}
+    character in a particular case, obtain the caseless ASCII character
+    it represents -}
 toCaselessChar :: CharSuperset superset =>
     ASCII'case letterCase superset {- ^ Character that is known to be ASCII, and
                                         in the particular case if it is a letter -}
@@ -150,7 +147,7 @@ toCaselessChar :: CharSuperset superset =>
 toCaselessChar = Caseless.disregardCase . Superset.toCharUnsafe . lift
 
 {-| Given a character from a larger set that is known to represent an ASCII
-character, manipulate it as if it were an ASCII character -}
+    character, manipulate it as if it were an ASCII character -}
 asCaselessChar :: forall letterCase superset. KnownCase letterCase => CharSuperset superset =>
     (CaselessChar -> CaselessChar) -- ^ Case-insensitive function over ASCII characters
     -> ASCII'case letterCase superset {- ^ Character that is known to be ASCII, and
@@ -161,7 +158,7 @@ asCaselessChar f = asciiCaseUnsafe . Superset.asCharUnsafe g . lift
     g = Caseless.toCase (theCase @letterCase) . f . Caseless.assumeCaseUnsafe (theCase @letterCase)
 
 {-| Given an ASCII superset character that is known to be valid ASCII,
-refine it further by converting it to a particular letter case -}
+    refine it further by converting it to a particular letter case -}
 refineCharToCase :: forall letterCase char. KnownCase letterCase => CharSuperset char =>
     ASCII char -> ASCII'case letterCase char
 refineCharToCase = asciiCaseUnsafe . Superset.toCaseChar (theCase @letterCase) . Refinement.lift
@@ -169,7 +166,7 @@ refineCharToCase = asciiCaseUnsafe . Superset.toCaseChar (theCase @letterCase) .
 ---
 
 {-| Return 'Just' an 'ASCII'case' string if the input consists entirely of ASCII
-characters in the proper case, or 'Nothing' otherwise -}
+    characters in the proper case, or 'Nothing' otherwise -}
 validateString :: forall letterCase superset. KnownCase letterCase => StringSuperset superset =>
     superset -- ^ String which may or may not be valid ASCII, where letters may be in any case
     -> Maybe (ASCII'case letterCase superset)
@@ -179,22 +176,22 @@ validateString x = do
     Just (asciiCaseUnsafe x)
 
 {-| Lift a list of 'CaselessChar' into a superset string type, wrapped in the
-'ASCII'case' refinement to save the evidence that all of the characters in the
-string are ASCII in a particular case. -}
+    'ASCII'case' refinement to save the evidence that all of the characters in
+    the string are ASCII in a particular case -}
 fromCaselessCharList :: forall letterCase superset. KnownCase letterCase => StringSuperset superset =>
     [CaselessChar] -- ^ Case-insensitive ASCII string represented as a list of caseless characters
     -> ASCII'case letterCase superset
 fromCaselessCharList = asciiCaseUnsafe . Superset.fromCharList . List.map (Caseless.toCase (theCase @letterCase))
 
-{-| Given a string from some type that is known to represent only ASCII
-characters in a particular case, obtain the caseless characters it represents -}
+{-| Given a string from some type that is known to represent only ASCII characters
+    in a particular case, obtain the caseless characters it represents -}
 toCaselessCharList :: forall letterCase superset. KnownCase letterCase => StringSuperset superset =>
     ASCII'case letterCase superset -- ^ String that is known to be valid ASCII in a particular case
     -> [CaselessChar]
 toCaselessCharList = List.map (Caseless.assumeCaseUnsafe (theCase @letterCase)) . Superset.toCharListUnsafe . lift
 
 {-| Forces a string from a larger character set into cased ASCII by using the
-'ASCII.Substitute' character in place of any unacceptable characters. -}
+    'ASCII.Substitute' character in place of any unacceptable characters -}
 substituteString :: forall letterCase superset. KnownCase letterCase => StringSuperset superset =>
     superset -- ^ String which may or may not be valid ASCII, where letters may be in any case
     -> ASCII'case letterCase superset
@@ -205,8 +202,8 @@ substituteString = asciiCaseUnsafe . Superset.fromCharList . List.map f . Supers
           else x
 
 {-| Given a string from a larger set that is known to consist entirely of ASCII
-characters in a particular case, map over the characters in the string as if
-they were caseless ASCII characters -}
+    characters in a particular case, map over the characters in the string as if
+    they were caseless ASCII characters -}
 mapChars :: forall letterCase superset. KnownCase letterCase => StringSuperset superset =>
     (CaselessChar -> CaselessChar) -- ^ Case-insensitive function over ASCII characters
     -> ASCII'case letterCase superset -- ^ String that is known to be valid ASCII in a particular case
