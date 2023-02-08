d2d4085e25e530571542dc545b736a49dd4497cd
Chris Martin <ch.martin@gmail.com>
2023-02-07 18:08:19 -0700

    add StringSupersetConversion instances for [Unicode.Char]

--- ascii-superset/library/ASCII/SupersetConversion.hs
+++ ascii-superset/library/ASCII/SupersetConversion.hs
@@ -10,7 +10,10 @@ import ASCII.Superset
 import {-# source #-} ASCII.Refinement.Internal (convertRefinedString)
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS.Char8
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS.Char8
+import qualified Data.Char as Unicode
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
@@ -42,3 +45,29 @@ instance StringSupersetConversion BS.ByteString LBS.ByteString where
 
 instance StringSupersetConversion LBS.ByteString BS.ByteString where
     convertStringUnsafe = LBS.toStrict
+
+---
+
+instance StringSupersetConversion T.Text [Unicode.Char] where
+    convertStringUnsafe = T.unpack
+
+instance StringSupersetConversion [Unicode.Char] T.Text where
+    convertStringUnsafe = T.pack
+
+instance StringSupersetConversion LT.Text [Unicode.Char] where
+    convertStringUnsafe = LT.unpack
+
+instance StringSupersetConversion [Unicode.Char] LT.Text where
+    convertStringUnsafe = LT.pack
+
+instance StringSupersetConversion BS.ByteString [Unicode.Char] where
+    convertStringUnsafe = BS.Char8.unpack
+
+instance StringSupersetConversion [Unicode.Char] BS.ByteString where
+    convertStringUnsafe = BS.Char8.pack
+
+instance StringSupersetConversion LBS.ByteString [Unicode.Char] where
+    convertStringUnsafe = LBS.Char8.unpack
+
+instance StringSupersetConversion [Unicode.Char] LBS.ByteString where
+    convertStringUnsafe = LBS.Char8.pack
